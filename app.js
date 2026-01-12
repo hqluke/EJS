05cdf86d4aa8a0c490d51eb2b160f6c856b8b12b
hqluke <hqluke@users.noreply.github.com>
2026-01-11 21:38:08 -0700

    trying out ejs

---
+++ app.js
@@ -0,0 +1,27 @@
+// app.js
+const express = require("express");
+const app = express();
+const path = require("node:path");
+const links = [
+    { href: "/", text: "Home" },
+    { href: "about", text: "About" },
+];
+const users = ["Rose", "Cake", "Biff"];
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index", { links: links, users: users });
+});
+
+const PORT = 3000;
+app.listen(PORT, (error) => {
+    if (error) {
+        throw error;
+    }
+    console.log(`My first Express app - listening on port ${PORT}!`);
+});
